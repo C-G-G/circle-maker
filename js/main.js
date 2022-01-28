bc1be493a701d351180f57f355b181f6e7a27669
CGG <16348315+C-G-G@users.noreply.github.com>
2022-01-27 23:53:26 -0500

    Create div inside event listener function, append to body and add class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // var $html = $('html');
 var $body = $('body');
 
+// generate a random top and left coordinate for div element
+var randLeft = Math.random() * 1000;
+var randTop = Math.random() * 1000;
+
 // event listener that listens for keydown
 $body.on('keydown', function () {
 
   // create a new div
-  $body.append('<div class="ball"></div>');
+  var $div = $('<div>');
 
-  // generate a random top and left coordinate for div element
-  var randLeft = Math.random() * 1000;
-  var randTop = Math.random() * 1000;
+  $body.append($div).addClass('ball');
 
   // place div on the page on random position
   $('.ball').css('left', randLeft);
