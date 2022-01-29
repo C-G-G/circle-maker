b7bfb7478deb0af0dd4921754de692bc3c1a8fae
CGG <16348315+C-G-G@users.noreply.github.com>
2022-01-29 14:41:42 -0500

    Put randomization lines inside keydown function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 // var $html = $('html');
 var $body = $('body');
 
-// generate a random top and left coordinate for div element
-var randLeft = Math.random() * 1000;
-var randTop = Math.random() * 1000;
-
 // event listener that listens for keydown
 $body.on('keydown', function () {
 
   // create a new div
   var $div = $('<div>');
 
-  // add div to body, then assign a class 
+  // add div to body, then assign a class
   $body.append($div);
   $div.addClass('ball');
 
+  // generate random top and left coordinate for div element
+  // var randLeft = Math.random() * 1000;
+  // var randTop = Math.random() * 1000;
+
   // place div on the page on random position
-  // $('.ball').css('left', randLeft);
-  // $('.ball').css('top', randTop);
-  $div.css('left', randLeft);
-  $div.css('top', randTop);
+  $div.css('left', Math.random() * 1000);
+  $div.css('top', Math.random() * 1000);
 
 });
